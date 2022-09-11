69d945f61e3b8fa87a418a581e77881e5f9f4b2a
Leandro Ramos <leandroramos@Leandros-MacBook-Pro.local>
2022-09-11 15:31:35 -0300

    commit to upload focus location button place

--- Padaria-SwiftUI/App/LandingPage/LandingPageView.swift
+++ Padaria-SwiftUI/App/LandingPage/LandingPageView.swift
@@ -8,13 +8,56 @@ struct LandingPageView: View {
     @State var locationManager = CLLocationManager()
     
     var body: some View {
+        
         ZStack {
+            
             Color("background").edgesIgnoringSafeArea(.all)
+            
             MapView()
                 .navigationBarBackButtonHidden(true)
                 .environmentObject(mapViewModel)
                 .navigationTitle(UserPreferences.shared.hasUserName)
                 .edgesIgnoringSafeArea(.bottom)
+            
+            VStack {
+                
+                VStack(spacing: 0) {
+                    
+                    HStack {
+                        
+                        Image(systemName: "magnifyingglass")
+                            .foregroundColor(.gray)
+                        
+                        TextField("Nome da padaria...",
+                                  text: $mapViewModel.searchText)
+                        .colorScheme(.light)
+                        
+                    }
+                    .padding(.vertical, 10)
+                    .padding(.horizontal)
+                    .background(Color.white)
+                    
+                    Spacer()
+            
+            }
+                
+                VStack {
+                    
+                    Button(action: mapViewModel.focusOnLocation,
+                           label: {
+                        Image(systemName: "location.fill")
+                            .font(.title2)
+                            .padding(10)
+                            .background(Color.secondary)
+                            .clipShape(Circle())
+                    })
+                }
+                .frame(maxWidth: .infinity,
+                       alignment: .trailing)
+                .padding()
+                .padding(.bottom, 10)
+                
+            }
         }
         .onAppear {
             locationManager.delegate = mapViewModel
--- Padaria-SwiftUI/App/Map/ViewModel/MapViewModel.swift
+++ Padaria-SwiftUI/App/Map/ViewModel/MapViewModel.swift
@@ -4,7 +4,17 @@ import SwiftUI
 
 class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     @Published var mapView = MKMapView()
-    @Published var region: MKCoordinateRegion?
+    @Published var region: MKCoordinateRegion!
+    @Published var mapType: MKMapType = .standard
+    @Published var searchText = ""
     
-    
+    func focusOnLocation()
+    {
+        guard let region = region else {
+            return
+        }
+        
+        mapView.setRegion(region, animated: true)
+        mapView.setVisibleMapRect(mapView.visibleMapRect, animated: true)
+    }
 }
